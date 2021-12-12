901a4e4e53afbc922e6e2fb00c74da5a6abc66d4
raihandaffa303 <92065577+raihandaffa303@users.noreply.github.com>
2021-12-12 23:41:04 +0700

    MODUL5 DAFFA

---
+++ MODUL5_DAFFA/resources/views/home_daffa.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Klinik EAD</title>
+</head>
+<body>
+<div class="row">
+        <center><h3 class="text-center mt-5 mb-2">About Us</h3></center>
+        <div class="col-6 text-justify">
+            <img src="image/vaksin.jpg" alt="">
+        </div>
+        <div class="col-6 text-justify">
+          <p>
+            Jangan ragu untuk datang ke klinik EAD karena murah dan berkualitas. Klinik EAD merupakan klinik yang bisa menyembuhkan segala bentuk penyakit. klinik EAD sendiri
+            sudah beridiri sejak tahun 2012 dan sudah memiliki beberapa keunggulan. Jangan ragu untuk datang
+            ke klinik EAD karena sudah banyak pasien yang sembuh.
+          </p>
+        </div>
+      </div>
+      <footer>
+    <div class="footer bg-info">
+        <h6 style="text-align: center;" class="m-2">©2021 Copyright: <a type="button" data-bs-toggle="modal" data-bs-target="#mymodal">Daffa_1202190181</a></h6>
+      </div>
+
+    <!-- Modal CREATED BY -->
+    
+        <style>
+    .footer {
+      width: 100%;
+      height: 48px;
+      background: #8db7f4;
+      position: bottom;
+      bottom: 0px;
+      left: 0;
+    }
+  </style>
+</body>
+</html>
+
